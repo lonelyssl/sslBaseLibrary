115358de2f09820dcc0e9a4f2c45f21f817c2ab0
slshi3 <slshi3@iflytek.com>
2019-06-13 20:50:45 +0800

    导入lib

--- app/src/main/java/com/ssl/base/app/MainActivity.kt
+++ app/src/main/java/com/ssl/base/app/MainActivity.kt
@@ -2,11 +2,13 @@ package com.ssl.base.app
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.ssl.common.utils.LogUtil
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        LogUtil.e("测试")
     }
 }
